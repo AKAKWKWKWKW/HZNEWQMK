e40de8b0c44f5b54cb648c34696420c53c31883c
AKAKWKWKWKW <136645694+AKAKWKWKWKW@users.noreply.github.com>
2023-06-17 23:11:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import telebot
+from telebot import types
+
+admin = 994421639
+
+bot_name = "VIP_SmS_RoBot"
+
+bot_token='1295995478:AAHSceSJgMBB16dWWU61E9wHpKBMz6NsrME'
+
+supp = 't.me/VIP_SmS_RoBot'
+subid =  -1001334740227
